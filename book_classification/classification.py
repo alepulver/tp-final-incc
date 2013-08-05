0b00faef1240d1c1c639147fd8610e7de14337cf
Alejandro Pulver <alepulver@gmail.com>
2013-08-04 23:28:54 -0300

    Seems to work with latest sklearn, but needs testing and proper model evaluation

--- book_classification/classification.py
+++ book_classification/classification.py
@@ -1,15 +1,20 @@
 from sklearn import svm
+from sklearn.decomposition import TruncatedSVD
 import book_classification as bc
 from scipy import sparse
 
 
 # TODO: feature aggregator for collectionfeatures, or a collectionfeatures aggregator for classificationmodel
 
-class ReducedMatrixBuilder:
+class MatrixTransformer:
 	def __init__(self, matrix_builder):
 		self._matrix_builder = matrix_builder
 	def for_collection(self, collection):
 		pass
+	def for_training_set(self):
+		pass
+	def decode_authors(self, sequence):
+		return self._matrix_builder.decode_authors(sequence)
 
 class MatrixBuilder:
 	def __init__(self, collection, features_extractor):
@@ -21,18 +26,20 @@ class MatrixBuilder:
 		authors = self._collection.authors()
 		self._authors_indexer = bc.NumericIndexer.from_objects(authors)
 
-		self._matrix = self.build_data_matrix(collection)
+		self._data_matrix = self.build_data_matrix(collection)
+		self._authors_matrix = self.build_authors_matrix(collection)
+		self._svd = TruncatedSVD(50).fit(self._data_matrix)
 
 	def for_collection(self, collection):
-		data = self.build_data_matrix(collection)
-		authors = build_authors_matrix(collection)
+		data = self._svd.transform(self.build_data_matrix(collection))
+		authors = self.build_authors_matrix(collection)
 		return data, authors
 	def for_training_set(self):
-		return self._matrix
+		return self._svd.transform(self._data_matrix), self._authors_matrix
 
 	def build_data_matrix(self, collection):
-		if collection == self._collection:
-			return self._matrix
+		#if collection == self._collection:
+		#	return self._matrix
 
 		collection_features = {}
 		for book in collection.books():
@@ -41,12 +48,13 @@ class MatrixBuilder:
 		matrix = sparse.dok_matrix((len(collection), len(self._features_indexer)))
 
 		for i,book in enumerate(collection.books()):
-			features = another_collection_features[book]
+			features = collection_features[book]
 			for k,v in features.items():
 				j = self._features_indexer.encode(k)
 				matrix[i, j] = v
 
-		return matrix.tocsc()
+		#return matrix.tocsc()
+		return matrix
 
 	def build_authors_matrix(self, collection):
 		result = []
@@ -59,13 +67,15 @@ class MatrixBuilder:
 
 class ClassificationModel:
 	def __init__(self, matrix_builder, model_class):
+		self._matrix_builder = matrix_builder
 		self._model = model_class()
 		data, authors = self._matrix_builder.for_training_set()
 		self._model.fit(data, authors)
 
 	def classify(self, collection):
 		#book_indexer = bc.NumericIndexer.from_objs(collection.books())
-		result = self._model.predict(self._matrix_builder.for_collection(collection))
+		data, authors = self._matrix_builder.for_collection(collection)
+		result = self._model.predict(data)
 		# decode output to book collection with possibly wrong authors
 		return self._matrix_builder.decode_authors(result)
 
