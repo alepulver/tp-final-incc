8b08f285205aa59bfbffa22cf976109693c21ed1
Alejandro Pulver <alejandro@8thlight.com>
2013-09-18 15:19:33 -0300

    ...

--- travis.sh
+++ travis.sh
@@ -1,6 +1,8 @@
-sudo apt-get install libblas-dev libatlas-dev liblapack-dev gfortran
+#sudo apt-get install libblas-dev libatlas-dev liblapack-dev gfortran
 
 # XXX: not recognized by scipy if not installed before
-pip install 'numpy>=1.3'
+#pip install 'numpy>=1.3'
+
+sudo apt-get install python3-numpy python3-scipy
 
 pip install -r requirements.txt --use-mirrors
