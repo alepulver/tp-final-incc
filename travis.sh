df56c4c4a035b9d36c3390a7f03624aa56af87c4
Alejandro Pulver <alejandro@8thlight.com>
2013-09-18 14:31:24 -0300

    Install numpy before scipy

--- travis.sh
+++ travis.sh
@@ -1,2 +1,6 @@
 sudo apt-get install libblas-dev libatlas-dev liblapack-dev gfortran
+
+# XXX: not recognized by scipy if not installed before
+pip install 'numpy>=1.3'
+
 pip install -r requirements.txt --use-mirrors
