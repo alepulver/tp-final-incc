f0d7ee7924bf22155bfbbaf52408ed6680f9ad15
Alejandro Pulver <alepulver@gmail.com>
2013-09-19 13:58:18 -0300

    Don't use fancy nose stuff in Travis

--- travis-install.sh
+++ travis-install.sh
@@ -12,6 +12,8 @@ source /home/ale/Programs/my-python3-env/bin/activate
 rm -rf $VIRTUAL_ENV
 sudo ln -s /home/ale/Programs/my-python3-env $VIRTUAL_ENV
 
+mv setup.cfg setup.cfg.bak
+
 #sudo apt-get install libblas-dev libatlas-dev liblapack-dev gfortran
 
 #pip install -r requirements.txt --use-mirrors
