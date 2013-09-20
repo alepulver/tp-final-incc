e6c37c9a20740b68e7f37ab0b8c3fd9525a8eefd
Alejandro Pulver <alejandro@8thlight.com>
2013-09-20 14:15:36 -0300

    Finally?

--- travis-install.sh
+++ travis-install.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
 
 sudo apt-get install lrzip
-wget 'https://www.dropbox.com/s/nkh802pdyi5bvyk/my-python3-env.tar.lrz'
+wget 'https://www.dropbox.com/s/fmp5ew0vzdd7bk1/my-python3-env.tar.lrz'
 lrzuntar my-python3-env.tar.lrz
-
-sudo mkdir -p /home/ale/Programs
-sudo chown $USER:$USER /home/ale/Programs
-mv my-python3-env /home/ale/Programs
-
-#source /home/ale/Programs/my-python3-env/bin/activate
-
-#rm -rf $VIRTUAL_ENV
-#sudo ln -s /home/ale/Programs/my-python3-env $VIRTUAL_ENV
 
 mv setup.cfg setup.cfg.bak
 
-sudo apt-get install python3-pkg-resources
-
 #sudo apt-get install libblas-dev libatlas-dev liblapack-dev gfortran
-
 #pip install -r requirements.txt --use-mirrors
