03b356ee4c11031e2e5575b7e273f476284010b3
Alejandro Pulver <alejandro@8thlight.com>
2013-09-20 14:22:19 -0300

    ...

--- travis-install.sh
+++ travis-install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+mv setup.cfg setup.cfg.bak
+
+cd $HOME
+
 sudo apt-get install lrzip
 wget 'https://www.dropbox.com/s/fmp5ew0vzdd7bk1/my-python3-env.tar.lrz'
 lrzuntar my-python3-env.tar.lrz
-
-mv setup.cfg setup.cfg.bak
 
 #sudo apt-get install libblas-dev libatlas-dev liblapack-dev gfortran
 #pip install -r requirements.txt --use-mirrors
